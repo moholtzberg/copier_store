5b6337a811008f18650e5df5d83c3e7fa2da48a7
Moshe Holtzberg <moholtzberg@gmail.com>
2014-08-19 23:33:28 -0400

    Removed S3 config from initializers/spree.rb

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,10 +6,7 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  config.use_s3 = true
-  config.s3_bucket = ENV["S3_BUCKET"]
-  config.s3_access_key = ENV["S3_ACCESS_KEY"]
-  config.s3_secret = ENV["S3_SECRET"]
+
 end
 
 Spree.user_class = "Spree::LegacyUser"
